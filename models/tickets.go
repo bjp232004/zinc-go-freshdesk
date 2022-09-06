462409e7a4bbe3f67857c98b1c688385d811252c
Bhargav <BJP232004@GMAIL.COM>
2022-09-06 11:52:40 +0530

    Update tickets.go

--- models/tickets.go
+++ models/tickets.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 type Source uint8
 
 const (
@@ -43,16 +45,16 @@ type Ticket struct {
 	Email string `json:"email"`
 
 	// Facebook ID of the requester. A contact should exist with this facebook_id in Freshdesk.
-	// FacebookID string `json:"facebook_id"`
+	FacebookID string `json:"facebook_id,omitempty"`
 
 	// Phone number of the requester. If no contact exists with this phone number in Freshdesk, it will be added as a new contact. If the phone number is set and the email address is not, then the name attribute is mandatory.
-	// Phone string `json:"phone"`
+	Phone string `json:"phone,omitempty"`
 
 	// Twitter handle of the requester. If no contact exists with this handle in Freshdesk, it will be added as a new contact.
-	// TwitterID string `json:"twitter_id"`
+	TwitterID string `json:"twitter_id,omitempty"`
 
 	// External ID of the requester. If no contact exists with this external ID in Freshdesk, they will be added as a new contact.
-	// UniqueExternalID string `json:"unique_external_id"`
+	UniqueExternalID string `json:"unique_external_id,omitempty"`
 
 	// Subject of the ticket. The default Value is null.
 	Subject string `json:"subject"`
@@ -74,40 +76,40 @@ type Ticket struct {
 
 	// Ticket attachments. The total size of these attachments cannot exceed 15MB.
 	// Not implemented
-	// Attachments []interface{} `json:"attachments"`
+	Attachments []interface{} `json:"attachments,omitempty"`
 
 	// Email address added in the 'cc' field of the incoming ticket email
-	// CCEmails []string `json:"cc_emails"`
+	CCEmails []string `json:"cc_emails,omitempty"`
 
-	// FWDEmails []string `json:"fwd_emails"`
+	FWDEmails []string `json:"fwd_emails,omitempty"`
 
-	// REPLYCCEmails []string `json:"reply_cc_emails"`
+	REPLYCCEmails []string `json:"reply_cc_emails,omitempty"`
 
 	// Key value pairs containing the names and values of custom fields. Read more here
-	// CustomFields map[string]interface{} `json:"custom_fields"`
+	CustomFields map[string]interface{} `json:"custom_fields,omitempty"`
 
 	// Timestamp that denotes when the ticket is due to be resolved
-	// DueBy time.Time `json:"due_by"`
+	DueBy time.Time `json:"due_by,omitempty"`
 
 	// ID of email config which is used for this ticket. (i.e., support@yourcompany.com/sales@yourcompany.com)
 	// If product_id is given and email_config_id is not given, product's primary email_config_id will be set
-	// EmailConfigID uint64 `json:"email_config_id"`
+	EmailConfigID uint64 `json:"email_config_id,omitempty"`
 
 	// Timestamp that denotes when the first response is due
-	// FirstResponseDueBy time.Time `json:"fr_due_by"`
+	FirstResponseDueBy time.Time `json:"fr_due_by,omitempty"`
 
 	// ID of the group to which the ticket has been assigned. The default value is the ID of the group that is associated with the given email_config_id
-	// GroupID uint64 `json:"group_id"`
+	GroupID uint64 `json:"group_id,omitempty"`
 
 	// ID of the product to which the ticket is associated. It will be ignored if the email_config_id attribute is set in the request.
-	// ProductID uint64 `json:"product_id"`
+	ProductID uint64 `json:"product_id,omitempty"`
 
 	// The channel through which the ticket was created. The default value is 2.
 	Source Source `json:"source"`
 
 	// Tags that have been associated with the ticket
-	// Tags []string `json:"tags"`
+	Tags []string `json:"tags,omitempty"`
 
 	// Company ID of the requester. This attribute can only be set if the Multiple Companies feature is enabled (Estate plan and above)
-	// CompanyID uint64 `json:"company_id"`
+	CompanyID uint64 `json:"company_id,omitempty"`
 }
